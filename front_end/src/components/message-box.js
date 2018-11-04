b99c1f2df08e53dca622ea08b0c81bc99fe40125
cjmochrie <cameron.mochrie@gmail.com>
2018-11-04 12:02:02 -0500

    Fix issue with message box not clearing

--- front_end/src/components/message-box.js
+++ front_end/src/components/message-box.js
@@ -3,41 +3,33 @@ import { connect } from 'react-redux';
 import { Grid, Row, Col, Alert } from 'react-bootstrap';
 import { clearMessage } from '../actions';
 
-class MessageBox extends Component {
-
+class _MessageBox extends Component {
   componentDidMount() {
     // Automatically hide the message box after 5s
     setTimeout(() => this.props.clearMessage(), 5000);
   }
-
   render () {
     const message = this.props.message;
-    if (message.text) {
-      return (
-        <Grid>
-          <Row>
-            <Col md={6} xs={12} mdOffset={3}>
-              <Alert
-                bsStyle={message.type}
-                closeLabel="Close"
-                onDismiss={this.props.handleDismiss}>
-                {message.text}
-              </Alert>
-            </Col>
-          </Row>
-        </Grid>
-      );
-    }
-    
-      return <div />;
-    
+    return message.text ?
+      <Grid>
+        <Row>
+          <Col md={6} xs={12} mdOffset={3}>
+            <Alert
+              bsStyle={message.type}
+              closeLabel="Close"
+              onDismiss={this.props.handleDismiss}>
+              {message.text}
+            </Alert>
+          </Col>
+        </Row>
+      </Grid> :
+      <div />
   }
 }
 
-// MessageBox.propTypes = {
-//   message : PropTypes.object,
-//   handleDismiss : PropTypes.func
-// };
+const MessageBox = ({ message, clearMessage }) =>
+  message.text && <_MessageBox message={message} clearMessage={clearMessage} />;
+
 
 const mapStateToProps = state => ({
   message: state.message,
