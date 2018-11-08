7d7c92fdadc1e1200fff501354e7b88744bfd1d8
cjmochrie <cameron.mochrie@gmail.com>
2018-11-08 16:14:53 -0500

    Configure Sentry

--- stock_loan/extensions.py
+++ stock_loan/extensions.py
@@ -1,3 +1,4 @@
+import os
 import memcache
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -6,9 +7,15 @@ from flask_limiter.util import get_ipaddr
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from .borrow import Borrow
 
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_KEY').strip(),
+    integrations=[FlaskIntegration()],
+    environment=os.getenv('APP_ENV').strip(),
+)
 db = SQLAlchemy()
 limiter = Limiter(key_func=get_ipaddr)
 login_manager = LoginManager()
